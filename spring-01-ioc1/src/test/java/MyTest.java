27395fb9098bb300b9c3e691d6627dc505ba1aa2
a65625460 <806163009@qq.com>
2020-04-12 15:24:08 +0800

    spring-02-hellospring

--- spring-01-ioc1/src/test/java/MyTest.java
+++ spring-01-ioc1/src/test/java/MyTest.java
@@ -1,17 +1,22 @@
-import com.kuang.dao.UserDaoImpl;
-import com.kuang.dao.UserDaoMysqlImpl;
-import com.kuang.dao.UserDaoOracleImpl;
-import com.kuang.dao.UserDaoSqlserverImpl;
-import com.kuang.service.UserService;
 import com.kuang.service.UserServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     public static void main(String[] args) {
 
-        // 用户实际调用的是业务层，dao层他们不需要接触！
-        UserService userService = new UserServiceImpl();
+//        // 用户实际调用的是业务层，dao层他们不需要接触！
+//        UserService userService = new UserServiceImpl();
+//
+//        userService.setUserDao(new UserDaoSqlserverImpl());
+//        userService.getUser();
 
-        userService.setUserDao(new UserDaoSqlserverImpl());
-        userService.getUser();
+        // 获取ApplicationContext；拿到Spring的容器
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        // 容器在手，天下我有，需要什么，就直接get什么！
+        UserServiceImpl userServiceImpl = (UserServiceImpl) context.getBean("UserServiceImpl");
+
+        userServiceImpl.getUser();
     }
 }
