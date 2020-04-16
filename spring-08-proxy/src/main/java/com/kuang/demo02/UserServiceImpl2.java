c34f5384a9bb0e5120255b692849b62d838f1ee7
a65625460 <806163009@qq.com>
2020-04-16 11:12:15 +0800

    spring-08-proxy

---
+++ spring-08-proxy/src/main/java/com/kuang/demo02/UserServiceImpl2.java
@@ -0,0 +1,23 @@
+package com.kuang.demo02;
+
+public class UserServiceImpl2 implements UserService{
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void query() {
+
+    }
+}
